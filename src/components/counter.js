da0473403f9b706fcf06a4369e255fe9599a8042
RadekKosiada <kosiadar@gmail.com>
2019-09-29 20:33:27 +0200

    Add click event to Counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,11 +3,19 @@ import React from "react";
 class Counter extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0
+    }
+    this.addCounter = this.addCounter.bind(this);
+  }
+  addCounter() {
+    this.setState({count: this.state.count+1})
   }
   render() {
     return (
       <div>
-        <p>Hello</p>
+        <button onClick={this.addCounter} >Click me!</button>
+        <p>Counter: {this.state.count}</p>
       </div>
     );
   }
