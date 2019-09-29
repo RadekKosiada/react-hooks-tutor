26fb449b531f51ce23a527c0a65ad12584bcc566
RadekKosiada <kosiadar@gmail.com>
2019-09-29 21:25:28 +0200

    Add comments explaining useState()

--- src/components/counter.js
+++ src/components/counter.js
@@ -23,11 +23,22 @@ class Counter extends React.Component {
 }
 
 function Counter2() {
+  // declare a new state variable: count;
+  // useState is a new way to use the exact same capabilities that this.state provides in a class;
+  // state variables are preserved by React;
+  // we pass initial state as argument to useState();
+  // state doesnt't have to be an object!;
+  // [count, setCount] is like 'this.state.count' and 'this.setState' together;
+  // that means useState() returns current state AND a function that updates it;
+  const [count, setCount] = useState(0);
+  // to store two different values, we call useState() again;
+  const [fruit, setFruit] = useState('banana'); 
+
   return (
     <div className="with-hooks">
       <h3>FUNCTION COMPONENT</h3>
       <button >Click me!</button>
-      <p>Counter (WITH hooks): 0</p>
+      <p>Counter (WITH hooks): {count}</p>
     </div>
   );
 }
